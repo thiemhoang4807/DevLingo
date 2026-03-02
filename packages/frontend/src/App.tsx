d3515fddde7afe45eab4532fa426d70d2c37bf7f
Hua Nguyen Khoi <memuopmapdit1234@gmail.com>
2026-03-02 09:04:41 +0000

    feat: Creating a layouts folder to save layouts of pages, including Header folder  and Footer Footer folder. Installing lucid-react. They dont add effects.

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,14 +1,13 @@
-import './App.css'
+import Header from "./layouts/Header";
+import Footer from "./layouts/Footer";
+import "./App.css";
 
 function App() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <h1 className="text-4xl font-bold text-blue-600 mb-4">
-        🚀 DevLingo Frontend Ready!
-      </h1>
-      <button className="px-6 py-2 bg-green-500 text-white font-semibold rounded-lg shadow-md hover:bg-green-600 transition-all">
-        Tailwind is working perfectly
-      </button>
+    <div>
+      <Header />
+      {/* Body in here */}
+      <Footer />
     </div>
   )
 }
