026a23a81d12b52c7f46b472ec96e8f034fb898e
tothuongcau <ngocduong2812thll@gmail.com>
2026-04-11 18:19:30 +0700

    Add route to get user's badges

--- packages/backend/src/routes/badgeRoutes.ts
+++ packages/backend/src/routes/badgeRoutes.ts
@@ -1,10 +1,16 @@
 import { Router } from "express";
 import { uploadBadge } from "../middlewares/uploadMiddleware"; 
 import { verifyToken } from "../middlewares/authMiddleware";
-import { createBadge, getBadges, updateBadge, deleteBadge } from "../controllers/badgeController";
+import { 
+  createBadge, 
+  getBadges, 
+  updateBadge, 
+  deleteBadge,
+  getMyBadges 
+} from "../controllers/badgeController";
 
 const router = Router();
-
+router.get("/badges/me", verifyToken, getMyBadges);
 router.post("/badges", verifyToken, uploadBadge.single("image"), createBadge);
 router.get("/badges", verifyToken, getBadges);
 router.put("/badges/:id", verifyToken, uploadBadge.single("image"), updateBadge);
