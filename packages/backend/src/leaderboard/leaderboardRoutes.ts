a5130cbf690c76879994b99e4a8dff3b41d2e1a4
tothuongcau <ngocduong2812thll@gmail.com>
2026-04-11 23:14:23 +0700

    Delete packages/backend/src/leaderboard directory

--- packages/backend/src/leaderboard/leaderboardRoutes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { LeaderboardController } from "./leaderboardController";
-
-const router = Router();
-
-router.get("/", LeaderboardController.getLeaderboard);
-
-export default router;
