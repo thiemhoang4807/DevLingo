13fcbe5d02891ec92f15e66dbbaf0f5bb1a6a4ca
tothuongcau <ngocduong2812thll@gmail.com>
2026-04-18 22:55:50 +0700

    Update lessonRoutes.ts

--- packages/backend/src/routes/lessonRoutes.ts
+++ packages/backend/src/routes/lessonRoutes.ts
@@ -1,12 +1,12 @@
 import { Router } from "express";
 import { verifyToken } from "../middlewares/authMiddleware";
-import { uploadLesson } from "../middlewares/uploadMiddleware"; 
+import { upload } from "../middlewares/uploadMiddleware";
 import { getLessons, handleCreateLesson, handleDeleteLesson, handleUpdateLesson } from "../controllers/lessonController";
 
 const router = Router();
 
-router.post("/lessons", verifyToken, uploadLesson.single("thumbnail"), handleCreateLesson);
+router.post("/lessons", verifyToken, upload.single("thumbnail"), handleCreateLesson);
 router.delete("/lessons/:id", verifyToken, handleDeleteLesson);
-router.put("/lessons/:id", verifyToken, uploadLesson.single("thumbnail"), handleUpdateLesson);
+router.put("/lessons/:id", verifyToken, upload.single("thumbnail"), handleUpdateLesson);
 router.get("/lessons", getLessons);
 export default router;
