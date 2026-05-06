ee9549fb2070149717036f32d81ef840a9a3fb30
Lê Vũ Thiêm Hoàng <thiemhoang2007@gmail.com>
2026-05-06 22:40:43 +0700

    fix docker backend

--- packages/backend/src/server.ts
+++ packages/backend/src/server.ts
@@ -77,7 +77,7 @@ AppDataSource.initialize()
     console.log("🚀 Data Source has been initialized!");
     logger.info("Data Source has been initialized!");
 
-    app.listen(PORT, () => {
+    app.listen(PORT as number, "0.0.0.0", () => {
       console.log(`🚀 Server is running on port ${PORT}`);
       logger.info(`Server is running on port ${PORT}`);
     });
